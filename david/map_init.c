a607f5bdf479a10901ae2d25a1a07c9af6072af1
David Francois <dafranco@3E5.42angouleme.fr>
2023-07-07 15:57:23 +0200

    ok

--- david/map_init.c
+++ david/map_init.c
@@ -1,67 +1,46 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_init.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dafranco <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 15:14:09 by dafranco          #+#    #+#             */
+/*   Updated: 2023/07/07 15:42:07 by dafranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include"libcub.h"
 
-static char ***map_cpy(char ***map, char **blueprint)
+static ssize_t get_mapsize_y(const char **blueprint)
 {
-	int	i;
-	int	j;
+	ssize_t	i;
 
 	i = 0;
-	j = 0;
-	while(**blueprint)
-	{
-		*map[i][j] = blueprint[i][j];
-		j++;
-		if (*map[i][j] == '\n')
-		{
-			j = 0;
-			i++;
-		}
-	}
-	*map[i][j] = '\0';
-	return(map);
+	return (i);
 }
 
-static char *fill_box(char *box, char c)
+static ssize_t get_mapsize_x(const char *blueprint)
 {
-	int i;
+	ssize_t	i;
 
 	i = 0;
-	box = malloc(sizeof(char *) * 91) + 1;
-	while(i < 91)
+	while (blueprint[i] && blueprint[i] != '\n')
+		i++;
+	return (i);
+}
+
+static char	**map_len(const char **blueprint)
+{
+	static ssize_t	x;
+	static ssize_t	y;
+	int				i;
+	char			**map;
+
+	i = 0;
+	while (*blueprint)
 	{
-		box[i] = 'c';
+		if (get_mapsize_y(*blueprint[i]) > x)
+			x = get_mapsize(*blueprint[i]);
 		i++;
 	}
-	box[i] = '\n';
-	i++;
-}
-
-static char **map_build(char **map, char **blueprint)
-{
-	int	i;
-	char **map_flat;
-
-	i = 0;
-	map_flat = (char **)malloc(sizeof(char **) * 91) + 1;
-	if (!map_flat)
-		return (NULL);
-	while(i < 91)
-		map_flat[i] = fill_box(map_flat[i], *blueprint[i]);
-	if (i < 90)
-		map_flat[i][91] = '\0';
-	map_flat[i] = NULL;
-	return (map);
-}
-
-char ***map_init(char **blueprint, int size)
-{
-	char ***map;
-	int i;
-
-	i = 0;
-	map = malloc(sizeof(char ***) * size);
-	if(!map)
-		return(NULL);
-	map = map_cpy(map, blueprint);
-	map[i] = map_build(map[i], blueprint);
 }
