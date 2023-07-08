ff606c2c2139a8a57b101cd91ea6b4669e1bd8cd
David Francois <dafranco@1D3.42angouleme.fr>
2023-07-08 19:12:19 +0200

    ok

--- david/main.c
+++ david/main.c
@@ -6,21 +6,19 @@
 /*   By: dafranco <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 17:07:38 by dafranco          #+#    #+#             */
-/*   Updated: 2023/07/07 18:09:00 by dafranco         ###   ########.fr       */
+/*   Updated: 2023/07/07 23:49:04 by dafranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libcub.h"
 
-static char	**map_len(int fd);
+static void	map_len(char *argv);
+static char	**map_cpy(int fd, size_t x, size_t y);
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	(void)argv;
-	char **map;
-	int fd = open("test.txt", O_RDONLY);
-	map = map_len(fd);
+	map_len(argv[1]);
 	return(0);
 }
 /*
@@ -43,13 +41,36 @@ static ssize_t get_mapsize_x(const char *blueprint, int fd)
 }
 */
 
-static char	**map_len(int fd)
+static char	**map_cpy(int fd, size_t x, size_t y)
+{
+	char	**map;
+	int		i;
+
+	i = 0;
+	map = malloc(sizeof(char **) * y);
+	if (!map)
+		return (NULL);
+	while (i < (int)y)
+	{
+		map[i] = ft_calloc(sizeof(char *), x + 1);
+		map[i] = strdup(get_next_line(fd));
+		printf("%s", map[i]);
+		i++;
+	}
+	return (map);
+}
+
+static void	map_len(char *argv)
 {
 	static size_t		x;
 	static size_t		y;
-	char			*str = NULL;
-	char			**map = NULL;
+	int					fd;
+	char				*str;
+	char				**map;
 
+	fd = open(argv, O_RDONLY);
+	str = NULL;
+	map = NULL;
 	while (1)
 	{
 		str = get_next_line(fd);
@@ -59,6 +80,8 @@ static char	**map_len(int fd)
 			break ;
 		y++;
 	}
-	printf("x = %lu\ny = %lu\n", x, y);
-	return (map);
+	close(fd);
+	fd = open(argv, O_RDONLY);
+	map = map_cpy(fd, x, y);
+	return ;
 }
